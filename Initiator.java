1d6ccc47b4522851b919ff42a08a1e1bab2abfd4
Nkosinathi Mothoa <nmothoa360@gmail.com>
2018-10-03 15:07:44 +0200

    Add files via upload

---
+++ Initiator.java
@@ -0,0 +1,12 @@
+public class Initiator {
+
+    public static void main(String[] args) {
+
+        String value = "H E L L O  F R O M  J E N K I N S  ! ! !";
+
+        for(int Y = 1; Y < value.length(); Y++){
+
+            System.out.println(value.substring(0, Y));
+        }
+    }
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+make:
+	javac *.java
+
+run:
+	java Initiator
+
+clean:
+	rm *.class
